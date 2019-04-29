6dd331256b0c410807ea6e74150f998abab3f392
Max St <maxstouten@hotmail.com>
2019-04-29 18:51:40 +0200

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    
+    <h1>About</h1>
+    <p class="subheading">The best services</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum tenetur quis delectus facere explicabo sunt qui ea culpa, laborum eos quas aliquam dolorum esse eligendi optio eveniet voluptates blanditiis earum?</p>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+  head() {
+      return {
+          title: 'About | Max Stouten',
+          meta: [
+              { name: 'description', content: 'Antwerp-based Web developer'},
+              { name: 'keywords', content: 'web developer, developer, javascript, front end, front end web developer'},
+          ]
+      }
+  }
+
+
+}
+</script>
